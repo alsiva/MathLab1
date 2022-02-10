958fc4e19ff5bbb404edc50c17fbd421f817e609
alexi <alexivanovrus@gmail.com>
2022-02-10 20:25:11 +0300

    JacobiAnswer

--- src/main/java/JacobiAnswer.java
+++ src/main/java/JacobiAnswer.java
@@ -25,5 +25,25 @@ public class JacobiAnswer {
         this.iterations = iterations;
     }
 
+    private String pattern(double i, double roots, double errors) {
+        return String.format("│ %02.0f │ % -20." + 10 + "f │ % -20.16f │%n", i, roots, errors);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("--------------------------------\n")
+                .append("№i | корень (x)   |  отклонение\n")
+                .append("-------------------------------\n");
+
+        for (int i = 0; i < roots.getRowDimention(); ++i) {
+            builder.append(pattern(i+1, roots.get(i, 0), errors.get(i, 0)));
+        }
+        builder.append("---------------------------------\n")
+                .append("Итераций ").append(iterations);
+
+        return builder.toString();
+    }
 
 }
